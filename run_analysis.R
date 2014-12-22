45e296f2afb92e8cbff41cd5d57941dce0e8ebba
Jean Paul <jpu2000@gmail.com>
2014-12-21 22:07:43 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,24 @@
 #This script will read files from folders & merge them to create one single data frame
 library(plyr)
 #Folders and file names
-feature_path <- "UCI/HAR/Dataset/features.txt"
-activity_labels_path <- "UCI/HAR/Dataset/activity_labels.txt"
-x_train_path <- "UCI/HAR/Dataset/x_train.txt"
-y_train_path <- "UCI/HAR/Dataset/y_train.txt"
-subject_train_path <- "UCI/HAR/Dataset/subject_train.txt"
-x_test_path <- "UCI/HAR/Dataset/x_test.txt"
-y_test_path <- "UCI/HAR/Dataset/y_test.txt"
-subject_test_path <- "UCI/HAR/Dataset/subject_test.txt"
+directory <- "UCI\ HAR\ Dataset"
+feature_path <- paste(directory, "/features.txt", sep = "")
+activity_labels_path <- paste(directory, "/activity_labels.txt", sep = "")
+x_train_path <- paste(directory, "/X_train.txt", sep = "")
+y_train_path <- paste(directory, "/y_train.txt", sep = "")
+subject_train_path <- paste(directory, "/subject_train.txt", sep = "")
+x_test_path <- paste(directory, "/X_test.txt", sep = "")
+y_test_path <- paste(directory, "/y_test.txt", sep = "")
+subject_test_path <- paste(directory, "/subject_test.txt", sep = "")
 
 #loading data...
-features <- read.table(feature_path, colClasses = c("characters"))
-activity_labels <- read.table(activity_label_path, col.names = c("ActivityID", "Activity"))
+features <- read.table(feature_path, colClasses = c("character"))
+activity_labels <- read.table(activity_labels_path, col.names = c("ActivityID", "Activity"))
 x_train <- read.table(x_train_path)
 y_train <- read.table(y_train_path)
 subject_train <- read.table(subject_train_path)
 x_test <- read.table(x_test_path)
-y_test <- read.table(y_text_path)
+y_test <- read.table(y_test_path)
 subject_test <- read.table(subject_test_path)
 
 #Merging Training and Testing datasets
@@ -30,7 +31,7 @@ data_labels <- rbind(rbind(features, c(562, "Subject")), c(563, "ActivityID")) [
 names(combined_data) <- data_labels
 
 #Getting columns of interests (mean and standard deviation)
-combined_data_MS <- combined_data[, grep1("mean|sd|Subject|ActivityID", names(combined_data))]
+combined_data_MS <- combined_data[, grepl("mean|sd|Subject|ActivityID", names(combined_data))]
 
 # Naming data with descriptive activity names
 combined_data_MS <- join(combined_data_MS, activity_labels, by = "ActivityID", match = "first")
@@ -49,9 +50,9 @@ names(combined_data_MS) <- gsub("^t", "TimeDomain", names(combined_data_MS))
 names(combined_data_MS) <- gsub("^f", "FrequencyDomain", names(combined_data_MS))
 names(combined_data_MS) <- gsub("\\.mean", ".Mean",names(combined_data_MS))
 names(combined_data_MS) <- gsub("\\.std", ".StandardDeviation", names(combined_data_MS))
-names(combined_data_MS) <- gsub("Freq\\", "Frequency.", names(combined_data_MS))
+names(combined_data_MS) <- gsub("Freq\\.", "Frequency.", names(combined_data_MS))
 names(combined_data_MS) <- gsub("Freq$", "Frequency", names(combined_data_MS))
 
 #Creating a tidy dataset with average of variable for each activity and subject
-combined_data_avg <- ddplyr(combined_data_MS, c("Subject", "Activity"), numcolwise(mean))
+combined_data_avg <- ddply(combined_data_MS, c("Subject", "Activity"), numcolwise(mean))
 write.table(combined_data_avg, file = "combined_data_avg.txt")
